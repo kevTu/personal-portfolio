8e291266fa63ca7c900f8c9d8350700b06960c90
kevintu <kgtu154@gmail.com>
2020-06-10 23:42:34 -0400

    removed quote italicization, added about section, updated banner with parallax effect, and implemented smooth scrolling for nav links

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+// Smooth-scrolling effect for links in navigation bar.
+
+$("#mainNav a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+        });
+    }
+});
