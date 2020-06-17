d51b980ebc2b1d7f8a5f1d6934eb76b35aa116dc
kevintu <kgtu154@gmail.com>
2020-06-17 00:00:57 -0400

    implemented navbar slide down on scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,4 @@
 // Smooth-scrolling effect for links in navigation bar.
-
 $("#mainNav a").on('click', function (event) {
     if (this.hash !== "") {
         event.preventDefault();
@@ -11,4 +10,9 @@ $("#mainNav a").on('click', function (event) {
         }, 800, function () {
         });
     }
+});
+
+// Slide down navigation bar on scroll.
+$(window).scroll(function () {
+    $("#mainNav").toggleClass("scrolling", $(this).scrollTop() > $("#mainNav").height());
 });
